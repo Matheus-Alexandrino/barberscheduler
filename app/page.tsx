744ecd75b20b1cd938d05b14189414aec6019a41
Matheus-Alexandrino <matwsalmeida@gmail.com>
2024-08-06 23:57:55 -0300

    chjore add git hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "./_components/ui/button";
 
-
 const Home = () => {
-  return <Button>text</Button>
-}
+  return <Button>text</Button>;
+};
 export default Home;
